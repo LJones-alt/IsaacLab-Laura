isaac-sim/IsaacLab

commit 59493b89adcf7d564c722645d6cc96f1415b047d

Fixes the rendering logic inside the environment classes (#515)
Fixes #476

The parameter substeps in the Simulation config had no influence on the
rendering frequency.
Additionally, app.update was not properly called when initializing the
stage due to the render method overload. This led to problems when
modifying the render interval.

- Merged physics stepping and rendering inside the step method in the
simulation config.
- Renamed substeps to render_interval in the simulation config.
- Updated all the tasks

## Type of change

- Breaking change (fix or feature that would cause existing
functionality to not work as expected)

## Checklist

- [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
`./isaaclab.sh --format`
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [x] I have added tests that prove my fix is effective or that my
feature works
- [x] I have run all the tests with `./isaaclab.sh --test` and they pass
- [x] I have updated the changelog and the corresponding version in the
extension's `config/extension.toml` file
- [x] I have added my name to the `CONTRIBUTORS.md` or my name already
exists there

source/extensions/omni.isaac.lab/config/extension.toml
@@ -1,7 +1,7 @@
 [package]
 
 # Note: Semantic Versioning is used: https://semver.org/
-version = "0.17.13"
+version = "0.18.0"
 
 # Description
 title = "Isaac Lab framework for Robot Learning"

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/direct_rl_env.py
@@ -16,6 +16,7 @@ from abc import abstractmethod
 from collections.abc import Sequence
 from typing import Any, ClassVar
 
+import carb
 import omni.isaac.core.utils.torch as torch_utils
 import omni.kit.app
 from omni.isaac.version import get_version
@@ -91,10 +92,18 @@ class DirectRLEnv(gym.Env):
         print("[INFO]: Base environment:")
         print(f"\tEnvironment device    : {self.device}")
         print(f"\tPhysics step-size     : {self.physics_dt}")
-        print(f"\tRendering step-size   : {self.physics_dt * self.cfg.sim.substeps}")
+        print(f"\tRendering step-size   : {self.physics_dt * self.cfg.sim.render_interval}")
         print(f"\tEnvironment step-size : {self.step_dt}")
         print(f"\tPhysics GPU pipeline  : {self.cfg.sim.use_gpu_pipeline}")
         print(f"\tPhysics GPU simulation: {self.cfg.sim.physx.use_gpu}")
+
+        if self.cfg.sim.render_interval < self.cfg.decimation:
+            msg = (
+                f"The render interval ({self.cfg.sim.render_interval}) is smaller than the decimation "
+                f"({self.cfg.decimation}). Multiple multiple render calls will happen for each environment step."
+                "If this is not intended, set the render interval to be equal to the decimation."
+            )
+            carb.log_warn(msg)
 
         # generate scene
         with Timer("[INFO]: Time taken for scene creation"):
@@ -146,6 +155,8 @@ class DirectRLEnv(gym.Env):
         self.extras = {}
 
         # initialize data and constants
+        # -- counter for simulation steps
+        self._sim_step_counter = 0
         # -- counter for curriculum
         self.common_step_counter = 0
         # -- init buffers
@@ -270,17 +281,18 @@ class DirectRLEnv(gym.Env):
         self._pre_physics_step(action)
         # perform physics stepping
         for _ in range(self.cfg.decimation):
+            self._sim_step_counter += 1
             # set actions into buffers
             self._apply_action()
             # set actions into simulator
             self.scene.write_data_to_sim()
+            render = self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
+                self.sim.has_gui() or self.sim.has_rtx_sensors()
+            )
             # simulate
-            self.sim.step(render=False)
+            self.sim.step(render=render)
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
-        # perform rendering if gui is enabled
-        if self.sim.has_gui() or self.sim.has_rtx_sensors():
-            self.sim.render()
 
         # post-step:
         # -- update env counters (used for curriculum generation)

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/manager_based_env.py
@@ -87,10 +87,21 @@ class ManagerBasedEnv:
         print("[INFO]: Base environment:")
         print(f"\tEnvironment device    : {self.device}")
         print(f"\tPhysics step-size     : {self.physics_dt}")
-        print(f"\tRendering step-size   : {self.physics_dt * self.cfg.sim.substeps}")
+        print(f"\tRendering step-size   : {self.physics_dt * self.cfg.sim.render_interval}")
         print(f"\tEnvironment step-size : {self.step_dt}")
         print(f"\tPhysics GPU pipeline  : {self.cfg.sim.use_gpu_pipeline}")
         print(f"\tPhysics GPU simulation: {self.cfg.sim.physx.use_gpu}")
+
+        if self.cfg.sim.render_interval < self.cfg.decimation:
+            msg = (
+                f"The render interval ({self.cfg.sim.render_interval}) is smaller than the decimation "
+                f"({self.cfg.decimation}). Multiple multiple render calls will happen for each environment step. "
+                "If this is not intended, set the render interval to be equal to the decimation."
+            )
+            carb.log_warn(msg)
+
+        # counter for simulation steps
+        self._sim_step_counter = 0
 
         # generate scene
         with Timer("[INFO]: Time taken for scene creation"):
@@ -253,17 +264,18 @@ class ManagerBasedEnv:
         self.action_manager.process_action(action)
         # perform physics stepping
         for _ in range(self.cfg.decimation):
+            self._sim_step_counter += 1
             # set actions into buffers
             self.action_manager.apply_action()
             # set actions into simulator
             self.scene.write_data_to_sim()
+            render = self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
+                self.sim.has_gui() or self.sim.has_rtx_sensors()
+            )
             # simulate
-            self.sim.step(render=False)
+            self.sim.step(render=render)
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
-        # perform rendering if gui is enabled
-        if self.sim.has_gui() or self.sim.has_rtx_sensors():
-            self.sim.render()
 
         # post-step: step interval event
         if "interval" in self.event_manager.available_modes:

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/manager_based_rl_env.py
@@ -154,17 +154,18 @@ class ManagerBasedRLEnv(ManagerBasedEnv, gym.Env):
         self.action_manager.process_action(action)
         # perform physics stepping
         for _ in range(self.cfg.decimation):
+            self._sim_step_counter += 1
             # set actions into buffers
             self.action_manager.apply_action()
             # set actions into simulator
             self.scene.write_data_to_sim()
+            render = self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
+                self.sim.has_gui() or self.sim.has_rtx_sensors()
+            )
             # simulate
-            self.sim.step(render=False)
+            self.sim.step(render=render)
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
-        # perform rendering if gui is enabled
-        if self.sim.has_gui() or self.sim.has_rtx_sensors():
-            self.sim.render()
 
         # post-step:
         # -- update env counters (used for curriculum generation)

source/extensions/omni.isaac.lab_tasks/config/extension.toml
@@ -1,7 +1,7 @@
 [package]
 
 # Note: Semantic Versioning is used: https://semver.org/
-version = "0.7.6"
+version = "0.7.7"
 
 # Description
 title = "Isaac Lab Environments"

source/extensions/omni.isaac.lab/omni/isaac/lab/sim/simulation_cfg.py
@@ -168,7 +168,7 @@ class SimulationCfg:
     dt: float = 1.0 / 60.0
     """The physics simulation time-step (in seconds). Default is 0.0167 seconds."""
 
-    substeps: int = 1
+    render_interval: int = 1
     """The number of physics simulation steps per rendering step. Default is 1."""
 
     gravity: tuple[float, float, float] = (0.0, 0.0, -9.81)

source/extensions/omni.isaac.lab/omni/isaac/lab/sim/simulation_context.py
@@ -214,7 +214,7 @@ class SimulationContext(_SimulationContext):
         super().__init__(
             stage_units_in_meters=1.0,
             physics_dt=self.cfg.dt,
-            rendering_dt=self.cfg.dt * self.cfg.substeps,
+            rendering_dt=self.cfg.dt * self.cfg.render_interval,
             backend="torch",
             sim_params=sim_params,
             physics_prim_path=self.cfg.physics_prim_path,
@@ -384,14 +384,14 @@ class SimulationContext(_SimulationContext):
                 self.render()
 
     def step(self, render: bool = True):
-        """Steps the physics simulation with the pre-defined time-step.
+        """Steps the simulation.
 
         .. note::
             This function blocks if the timeline is paused. It only returns when the timeline is playing.
 
         Args:
             render: Whether to render the scene after stepping the physics simulation.
-                If set to False, the scene is not rendered and only the physics simulation is stepped.
+                    If set to False, the scene is not rendered and only the physics simulation is stepped.
         """
         # check if the simulation timeline is paused. in that case keep stepping until it is playing
         if not self.is_playing():
@@ -467,6 +467,11 @@ class SimulationContext(_SimulationContext):
 
     def _init_stage(self, *args, **kwargs) -> Usd.Stage:
         _ = super()._init_stage(*args, **kwargs)
+        # a stage update here is needed for the case when physics_dt != rendering_dt, otherwise the app crashes
+        # when in headless mode
+        self.set_setting("/app/player/playSimulations", False)
+        self._app.update()
+        self.set_setting("/app/player/playSimulations", True)
         # set additional physx parameters and bind material
         self._set_additional_physx_params()
         # load flatcache/fabric interface

source/extensions/omni.isaac.lab/test/assets/check_fixed_base_assets.py
@@ -144,7 +144,7 @@ def main():
     """Main function."""
 
     # Initialize the simulation context
-    sim = sim_utils.SimulationContext(sim_utils.SimulationCfg(dt=0.01, substeps=1))
+    sim = sim_utils.SimulationContext(sim_utils.SimulationCfg(dt=0.01))
     # Set main camera
     sim.set_camera_view(eye=[2.5, 2.5, 2.5], target=[0.0, 0.0, 0.0])
     # design scene

LJones-alt/IsaacLab-Laura

commit 0760e82251a91396a499baf311aeafdec0e3be66

Fixes the rendering logic regression in environments (#614)
# Description

The previous rendering logic had a bug where a render call would also
step physics `render_interval` number of times.

This was a regression introduced in
59493b89adcf7d564c722645d6cc96f1415b047d

## Type of change

- Bug fix (non-breaking change which fixes an issue)

## Checklist

- [x] I have run the [`pre-commit` checks](https://pre-commit.com/) with
`./isaaclab.sh --format`
- [ ] I have made corresponding changes to the documentation
- [x] My changes generate no new warnings
- [x] I have added tests that prove my fix is effective or that my
feature works
- [x] I have updated the changelog and the corresponding version in the
extension's `config/extension.toml` file
- [x] I have added my name to the `CONTRIBUTORS.md` or my name already
exists there

source/extensions/omni.isaac.lab/config/extension.toml
@@ -1,7 +1,7 @@
 [package]
 
 # Note: Semantic Versioning is used: https://semver.org/
-version = "0.18.5"
+version = "0.18.6"
 
 # Description
 title = "Isaac Lab framework for Robot Learning"

source/extensions/omni.isaac.lab/omni/isaac/lab/app/app_launcher.py
@@ -534,7 +534,8 @@ class AppLauncher:
         for key, value in hacked_modules.items():
             sys.modules[key] = value
 
-    def _rendering_enabled(self):
+    def _rendering_enabled(self) -> bool:
+        """Check if rendering is required by the app."""
         # Indicates whether rendering is required by the app.
         # Extensions required for rendering bring startup and simulation costs, so we do not enable them if not required.
         return not self._headless or self._livestream >= 1 or self._enable_cameras

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/direct_rl_env.py
@@ -288,9 +288,13 @@ class DirectRLEnv(gym.Env):
         # add action noise
         if self.cfg.action_noise_model:
             action = self._action_noise_model.apply(action.clone())
-
         # process actions
         self._pre_physics_step(action)
+
+        # check if we need to do rendering within the physics loop
+        # note: checked here once to avoid multiple checks within the loop
+        is_rendering = self.sim.has_gui() or self.sim.has_rtx_sensors()
+
         # perform physics stepping
         for _ in range(self.cfg.decimation):
             self._sim_step_counter += 1
@@ -298,11 +302,13 @@ class DirectRLEnv(gym.Env):
             self._apply_action()
             # set actions into simulator
             self.scene.write_data_to_sim()
-            render = self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
-                self.sim.has_gui() or self.sim.has_rtx_sensors()
-            )
             # simulate
-            self.sim.step(render=render)
+            self.sim.step(render=False)
+            # render between steps only if the GUI or an RTX sensor needs it
+            # note: we assume the render interval to be the shortest accepted rendering interval.
+            #    If a camera needs rendering at a faster frequency, this will lead to unexpected behavior.
+            if self._sim_step_counter % self.cfg.sim.render_interval == 0 and is_rendering:
+                self.sim.render()
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
 
@@ -423,6 +429,8 @@ class DirectRLEnv(gym.Env):
     def close(self):
         """Cleanup for the environment."""
         if not self._is_closed:
+            # close entities related to the environment
+            # note: this is order-sensitive to avoid any dangling references
             if self.cfg.events:
                 del self.event_manager
             del self.scene

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/manager_based_env.py
@@ -262,6 +262,11 @@ class ManagerBasedEnv:
         """
         # process actions
         self.action_manager.process_action(action)
+
+        # check if we need to do rendering within the physics loop
+        # note: checked here once to avoid multiple checks within the loop
+        is_rendering = self.sim.has_gui() or self.sim.has_rtx_sensors()
+
         # perform physics stepping
         for _ in range(self.cfg.decimation):
             self._sim_step_counter += 1
@@ -269,11 +274,13 @@ class ManagerBasedEnv:
             self.action_manager.apply_action()
             # set actions into simulator
             self.scene.write_data_to_sim()
-            render = self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
-                self.sim.has_gui() or self.sim.has_rtx_sensors()
-            )
             # simulate
-            self.sim.step(render=render)
+            self.sim.step(render=False)
+            # render between steps only if the GUI or an RTX sensor needs it
+            # note: we assume the render interval to be the shortest accepted rendering interval.
+            #    If a camera needs rendering at a faster frequency, this will lead to unexpected behavior.
+            if self._sim_step_counter % self.cfg.sim.render_interval == 0 and is_rendering:
+                self.sim.render()
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
 

source/extensions/omni.isaac.lab/omni/isaac/lab/envs/manager_based_rl_env.py
@@ -154,6 +154,11 @@ class ManagerBasedRLEnv(ManagerBasedEnv, gym.Env):
         """
         # process actions
         self.action_manager.process_action(action)
+
+        # check if we need to do rendering within the physics loop
+        # note: checked here once to avoid multiple checks within the loop
+        is_rendering = self.sim.has_gui() or self.sim.has_rtx_sensors()
+
         # perform physics stepping
         for _ in range(self.cfg.decimation):
             self._sim_step_counter += 1
@@ -161,11 +166,13 @@ class ManagerBasedRLEnv(ManagerBasedEnv, gym.Env):
             self.action_manager.apply_action()
             # set actions into simulator
             self.scene.write_data_to_sim()
-            render = self._sim_step_counter % self.cfg.sim.render_interval == 0 and (
-                self.sim.has_gui() or self.sim.has_rtx_sensors()
-            )
             # simulate
-            self.sim.step(render=render)
+            self.sim.step(render=False)
+            # render between steps only if the GUI or an RTX sensor needs it
+            # note: we assume the render interval to be the shortest accepted rendering interval.
+            #    If a camera needs rendering at a faster frequency, this will lead to unexpected behavior.
+            if self._sim_step_counter % self.cfg.sim.render_interval == 0 and is_rendering:
+                self.sim.render()
             # update buffers at sim dt
             self.scene.update(dt=self.physics_dt)
 

source/extensions/omni.isaac.lab/test/envs/test_env_rendering_logic.py
@@ -1,0 +1,196 @@
+# Copyright (c) 2022-2024, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Launch Isaac Sim Simulator first."""
+
+from omni.isaac.lab.app import AppLauncher, run_tests
+
+# launch omniverse app
+# need to set "enable_cameras" true to be able to do rendering tests
+app_launcher = AppLauncher(headless=True, enable_cameras=True)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+
+import torch
+import unittest
+
+import omni.usd
+
+from omni.isaac.lab.envs import (
+    DirectRLEnv,
+    DirectRLEnvCfg,
+    ManagerBasedEnv,
+    ManagerBasedEnvCfg,
+    ManagerBasedRLEnv,
+    ManagerBasedRLEnvCfg,
+)
+from omni.isaac.lab.scene import InteractiveSceneCfg
+from omni.isaac.lab.sim import SimulationCfg, SimulationContext
+from omni.isaac.lab.utils import configclass
+
+
+@configclass
+class EmptyManagerCfg:
+    """Empty specifications for the environment."""
+
+    pass
+
+
+def create_manager_based_env(render_interval: int):
+    """Create a manager based environment."""
+
+    @configclass
+    class EnvCfg(ManagerBasedEnvCfg):
+        """Configuration for the test environment."""
+
+        decimation: int = 4
+        sim: SimulationCfg = SimulationCfg(dt=0.005, render_interval=render_interval)
+        scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=1, env_spacing=1.0)
+        actions: EmptyManagerCfg = EmptyManagerCfg()
+        observations: EmptyManagerCfg = EmptyManagerCfg()
+
+    return ManagerBasedEnv(cfg=EnvCfg())
+
+
+def create_manager_based_rl_env(render_interval: int):
+    """Create a manager based RL environment."""
+
+    @configclass
+    class EnvCfg(ManagerBasedRLEnvCfg):
+        """Configuration for the test environment."""
+
+        decimation: int = 4
+        sim: SimulationCfg = SimulationCfg(dt=0.005, render_interval=render_interval)
+        scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=1, env_spacing=1.0)
+        actions: EmptyManagerCfg = EmptyManagerCfg()
+        observations: EmptyManagerCfg = EmptyManagerCfg()
+
+    return ManagerBasedRLEnv(cfg=EnvCfg())
+
+
+def create_direct_rl_env(render_interval: int):
+    """Create a direct RL environment."""
+
+    @configclass
+    class EnvCfg(DirectRLEnvCfg):
+        """Configuration for the test environment."""
+
+        decimation: int = 4
+        num_actions: int = 0
+        num_observations: int = 0
+        sim: SimulationCfg = SimulationCfg(dt=0.005, render_interval=render_interval)
+        scene: InteractiveSceneCfg = InteractiveSceneCfg(num_envs=1, env_spacing=1.0)
+
+    class Env(DirectRLEnv):
+        """Test environment."""
+
+        def _pre_physics_step(self, actions):
+            pass
+
+        def _apply_action(self):
+            pass
+
+        def _get_observations(self):
+            return {}
+
+        def _get_rewards(self):
+            return {}
+
+        def _get_dones(self):
+            return torch.zeros(1, dtype=torch.bool), torch.zeros(1, dtype=torch.bool)
+
+    return Env(cfg=EnvCfg())
+
+
+class TestEnvRenderingLogic(unittest.TestCase):
+    """Test the rendering logic of the different environment workflows."""
+
+    def _physics_callback(self, dt):
+        # called at every physics step
+        self.physics_time += dt
+        self.num_physics_steps += 1
+
+    def _render_callback(self, event):
+        # called at every render step
+        self.render_time += event.payload["dt"]
+        self.num_render_steps += 1
+
+    def test_env_rendering_logic(self):
+        for env_type in ["manager_based_env", "manager_based_rl_env", "direct_rl_env"]:
+            for render_interval in [1, 2, 4, 8, 10]:
+                with self.subTest(env_type=env_type, render_interval=render_interval):
+                    # time tracking variables
+                    self.physics_time = 0.0
+                    self.render_time = 0.0
+                    # step tracking variables
+                    self.num_physics_steps = 0
+                    self.num_render_steps = 0
+
+                    # create a new stage
+                    omni.usd.get_context().new_stage()
+
+                    # create environment
+                    if env_type == "manager_based_env":
+                        env = create_manager_based_env(render_interval)
+                    elif env_type == "manager_based_rl_env":
+                        env = create_manager_based_rl_env(render_interval)
+                    else:
+                        env = create_direct_rl_env(render_interval)
+
+                    # enable the flag to render the environment
+                    # note: this is only done for the unit testing to "fake" camera rendering.
+                    #   normally this is set to True when cameras are created.
+                    env.sim.set_setting("/isaaclab/render/rtx_sensors", True)
+
+                    # disable the app from shutting down when the environment is closed
+                    # FIXME: Why is this needed in this test but not in the other tests?
+                    #   Without it, the test will exit after the environment is closed
+                    env.sim._app_control_on_stop_handle = None  # type: ignore
+
+                    # check that we are in partial rendering mode for the environment
+                    # this is enabled due to app launcher setting "enable_cameras=True"
+                    self.assertEqual(env.sim.render_mode, SimulationContext.RenderMode.PARTIAL_RENDERING)
+
+                    # add physics and render callbacks
+                    env.sim.add_physics_callback("physics_step", self._physics_callback)
+                    env.sim.add_render_callback("render_step", self._render_callback)
+
+                    # create a zero action tensor for stepping the environment
+                    actions = torch.zeros((env.num_envs, 0), device=env.device)
+
+                    # run the environment and check the rendering logic
+                    for i in range(50):
+                        # apply zero actions
+                        env.step(action=actions)
+
+                        # check that we have completed the correct number of physics steps
+                        self.assertEqual(
+                            self.num_physics_steps, (i + 1) * env.cfg.decimation, msg="Physics steps mismatch"
+                        )
+                        # check that we have simulated physics for the correct amount of time
+                        self.assertAlmostEqual(
+                            self.physics_time, self.num_physics_steps * env.cfg.sim.dt, msg="Physics time mismatch"
+                        )
+
+                        # check that we have completed the correct number of rendering steps
+                        self.assertEqual(
+                            self.num_render_steps,
+                            (i + 1) * env.cfg.decimation // env.cfg.sim.render_interval,
+                            msg="Render steps mismatch",
+                        )
+                        # check that we have rendered for the correct amount of time
+                        self.assertAlmostEqual(
+                            self.render_time,
+                            self.num_render_steps * env.cfg.sim.dt * env.cfg.sim.render_interval,
+                            msg="Render time mismatch",
+                        )
+
+                    # close the environment
+                    env.close()
+
+
+if __name__ == "__main__":
+    run_tests()